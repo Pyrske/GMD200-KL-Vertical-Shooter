7fde7fdf5d2ca80c25179e7ac6da3aa6e3751bc7
Pyrske <kalib.laclair@mymail.champlain.edu>
2024-02-01 12:43:57 -0500

    Created a damage animation, but it doesn't work yet

--- Veritcal Shooter_Kalib LaClair/Assets/Scripts/Ship.cs
+++ Veritcal Shooter_Kalib LaClair/Assets/Scripts/Ship.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+    Animator _animator;
     [ContextMenu("Damage")]
+    private void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
     public void TakeDamage()
     {
         PlayerHealth.SetHealth(PlayerHealth.GetHealth()-1);
+        _animator.SetBool("TakingDamage", true);
     }
 }
